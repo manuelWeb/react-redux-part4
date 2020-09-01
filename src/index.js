12223799e21976af1f7e5819e45e80a41c4c6b71
Becquet <mdevries@domoti.fr>
2020-09-01 19:37:51 +0200

    bug posts is not defined

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './routes';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import thunk from "redux-thunk"
 
-import Routes from './routes';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import ReducerPosts from './ReducerPosts'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  posts: ReducerPosts
 });
 
 export default rootReducer;
